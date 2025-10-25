21e2cbcee1dc0e7bc526901edb0282c4b957f86d
JaganMohan <mohan.kreddy301@gmail.com>
2025-10-25 05:56:13 +0530

    Project setup

---
+++ Gallery-SwiftUI/Gallery-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gallery-SwiftUI
+//
+//  Created by JM_Reddy on 25/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gallery-SwiftUI/Gallery-SwiftUI/Gallery_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gallery_SwiftUIApp.swift
+//  Gallery-SwiftUI
+//
+//  Created by JM_Reddy on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Gallery_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
